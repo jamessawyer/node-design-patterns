3daea114fe6cf3ed4e85e9c5b56e2f0404fffbfe
jamessawyer <550961784@qq.com>
2024-09-11 22:12:17 +0800

    流：使用pipeline方法替换pipe方法，更好的错误处理

--- ch06-streams/08_transform_streams/03_filter_reduce/main.mjs
+++ ch06-streams/08_transform_streams/03_filter_reduce/main.mjs
@@ -2,12 +2,24 @@ import { createReadStream } from 'node:fs'
 import { parse } from 'csv-parse'
 import FilterByGroup from './filter-by-group.mjs'
 import SumProfit from './sum-profit.mjs'
+import { pipeline } from 'node:stream'
 
 // csvParser 是一种转换流实例
 const csvParser = new parse({ columns: true })
 
-createReadStream('data.csv')
-    .pipe(csvParser)
-    .pipe(new FilterByGroup('Italy'))
-    .pipe(new SumProfit())
-    .pipe(process.stdout)
+// pipe() 不容易进行错误处理
+// createReadStream('data.csv')
+//     .pipe(csvParser)
+//     .pipe(new FilterByGroup('Italy'))
+//     .pipe(new SumProfit())
+//     .pipe(process.stdout)
+
+// ✅ pipeline() 会自动处理错误和进行清理，比pipe()更好
+pipeline(
+    createReadStream('data.csv'),
+    csvParser,
+    new FilterByGroup('Italy'),
+    new SumProfit(),
+    process.stdout,
+    err => console.error(err)
+)
